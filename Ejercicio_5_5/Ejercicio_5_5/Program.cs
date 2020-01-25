c5d843ae5e94df8f9b1a4ffe20b779bed2b0bb89
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-01-25 10:39:45 -0400

    Este Imprime en pantalla un numero entero de un digito positivo

---
+++ Ejercicio_5_5/Ejercicio_5_5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Ejercicio_5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Entrada;
+            int Numero;
+
+            try
+            {
+                Console.Write("Ingrese un Numero entero de un digito Positivo: ");
+                Entrada = Console.ReadLine();
+                Numero = Convert.ToInt32(Entrada);
+
+                NumLetra(Numero);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error...");
+            }
+        }
+
+        public static void NumLetra(int Num)
+        {
+            switch (Num)
+            {
+                case 1:
+                    Console.WriteLine("Uno");
+                    break;
+                case 2:
+                    Console.WriteLine("Dos");
+                    break;
+                case 3:
+                    Console.WriteLine("Tres");
+                    break;
+                case 4:
+                    Console.WriteLine("Cuatro");
+                    break;
+                case 5:
+                    Console.WriteLine("Cinco");
+                    break;
+                case 6:
+                    Console.WriteLine("Seis");
+                    break;
+                case 7:
+                    Console.WriteLine("Siete");
+                    break;
+                case 8:
+                    Console.WriteLine("Ocho");
+                    break;
+                case 9:
+                    Console.WriteLine("Nueve");
+                    break;
+                default:
+                    Console.WriteLine("Solo se Permite de un digito Positivo");
+                    break;
+            }
+        }
+    }
+}
