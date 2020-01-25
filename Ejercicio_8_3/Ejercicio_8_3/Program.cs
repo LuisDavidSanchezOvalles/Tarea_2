87abaa4ba88c83beb59b08a4a2b54e6a3df97421
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-01-25 13:18:56 -0400

    Este Muestra Por pantalla la Hora minutos meridiano dia mes año

---
+++ Ejercicio_8_3/Ejercicio_8_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_8_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString("hh:mm tt  dd/MM/yyyy"));//Hora Minutos Pm/Am dia mes y año
+        }
+    }
+}
