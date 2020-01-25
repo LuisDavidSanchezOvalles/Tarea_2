2e092a9c7d3f885ed93b29b4e2175d19ab952048
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-01-25 13:24:20 -0400

    programa que le solicita dos cadenas y luego las muestra en orden alfabético.

---
+++ Ejercicios_8_5/Ejercicios_8_5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace Ejercicios_8_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList Arreglo = new ArrayList();
+            string Cadena1, Cadena2;
+
+            try
+            {
+                Console.Write("Ingrese una cadena:");
+                Cadena1 = Console.ReadLine();
+                Arreglo.Add(Cadena1);
+
+                Console.Write("Ingrese la siguiente cadena:");
+                Cadena2 = Console.ReadLine();
+                Arreglo.Add(Cadena2);
+
+                Console.WriteLine("Las Cadenas Ordenadas son:");
+                Console.WriteLine("");
+
+                Arreglo.Sort();
+
+                foreach (String Cadena in Arreglo)
+                {
+                    Console.WriteLine("{0}", Cadena);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error...");
+            }
+        }
+    }
+}
